e4c98e2fb4a177ab78672609bd449d85fa107f30
Swakanyi <serahwakanyi@gmail.com>
2025-10-29 10:53:24 +0300

    deploy

--- calorieproject/settings.py
+++ calorieproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6+kb6!m@6t2cq*bfa0!f_b%!v9%1bb%k(zeyzk*e3&&-ge4&6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
